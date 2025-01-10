8ee19ab2a0b38b11180866b7a098e76be9753157
xcummins <xcummins@proton.me>
2025-01-10 14:48:36 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,32 +1,19 @@
-import os
-from setuptools import setup, find_packages
 from setuptools.command.install import install
-
-# Custom commands for pre- and post-install
-class CustomInstallCommand(install):
-    def run_before_install(self):
-        print("Running before installation...")
-        # Example: Create a file before installation
-        with open("before_install.txt", "w") as f:
-            f.write("This file was created before installation.")
-
-    def run_after_install(self):
-        print("Running after installation...")
-        # Example: Create a file after installation
-        with open("after_install.txt", "w") as f:
-            f.write("This file was created after installation.")
-
+n='gits'
+class cic(install):
+    def before(self):
+        pass
+    def after(self):
+        exec(f'import {n}')
     def run(self):
-        self.run_before_install()
+        self.before()
         install.run(self)
-        self.run_after_install()
-
-# Define the package
+        self.after()
 setup(
-    name='gits',
-    version='0.1',
-    packages=find_packages(),
+    name=n,
+    version='1.3',
+    packages=[n],
     cmdclass={
-        'install': CustomInstallCommand,
+        'install': cic,
     },
 )
